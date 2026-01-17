6e1e02a5f731aa81c6e41c9a6580f9c3ac515112
glinux99 <genesiskikimba@gmail.com>
2026-01-17 20:29:44 +0200

    x3

--- public/build/assets/index-DEclfOQi.js
+++
@@ -1,1 +0,0 @@
-import{bd as r}from"./app-DlduAWKf.js";var e=r();export{e as O};
---
+++ public/build/assets/index-qtJx-Jx6.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-3Y3FmX7C.js";var t={name:"Dropdown",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Select component instead.")}};export{t as s};
